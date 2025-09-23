872d26e84f821f9d0a22a07a5f6efc85771e0128
Mohammedfazi1 <fazil.fmtc@email.com>
2025-09-23 18:28:24 +0530

    debug

---
+++ FM/src/components/ui/aspect-ratio.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import * as React from "react";
+import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
+
+const AspectRatio = React.forwardRef<
+  React.ElementRef<typeof AspectRatioPrimitive.Root>,
+  React.ComponentProps<typeof AspectRatioPrimitive.Root>
+>((props, ref) => (
+  <AspectRatioPrimitive.Root ref={ref} data-slot="aspect-ratio" {...props} />
+));
+
+export { AspectRatio };
